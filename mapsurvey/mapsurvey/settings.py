fe807e965503d32160518eae65ea7ab062ce44b9
ganja_san <konuchovartem@gmail.com>
2019-05-08 20:16:01 +0300

    add slider

--- mapsurvey/mapsurvey/settings.py
+++ mapsurvey/mapsurvey/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,7 @@ DATABASES = {
         'NAME': 'mapsurvey',
         'USER': 'postgres',
         'PASSWORD': 'adminadmin',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
